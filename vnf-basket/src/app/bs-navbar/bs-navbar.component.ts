124cf8d2ba6527e5e27cd2e47aec0623c5ae9320
Rizwan Rahim <rizwanrahim@users.noreply.github.com>
2024-02-17 19:28:13 +0530

    Add routes (#14)

--- vnf-basket/src/app/bs-navbar/bs-navbar.component.ts
+++ vnf-basket/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'bs-navbar',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NgbDropdownModule],
   templateUrl: './bs-navbar.component.html',
   styleUrl: './bs-navbar.component.css'
 })
