5130574b184ed5d9111bceeebde513d61b7dbf6c
Rizwan Rahim <rizwanrahim@users.noreply.github.com>
2024-02-17 15:42:58 +0530

    Add routes (#12)

--- vnf-basket/src/app/app.routes.ts
+++ vnf-basket/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { LoginComponent } from './login/login.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'shopping-cart', component: ShoppingCartComponent },
+    { path: 'check-out', component: CheckOutComponent },
+    { path: 'order-success', component: OrderSuccessComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'admin/products', component: AdminProductsComponent },
+    { path: 'admin/orders', component: AdminProductsComponent },
+];
