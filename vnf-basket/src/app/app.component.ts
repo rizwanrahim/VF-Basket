47586b60c4440996e33028d786371594b1d81b22
Rizwan Rahim <rizwanrahim@users.noreply.github.com>
2024-02-17 17:46:48 +0530

    Revert "Add routes (#12)"
    This reverts commit 5130574b184ed5d9111bceeebde513d61b7dbf6c.

--- vnf-basket/src/app/app.component.ts
+++ vnf-basket/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BsNavbarComponent } from "./bs-navbar/bs-navbar.component";
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, BsNavbarComponent]
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'vnf-basket';
