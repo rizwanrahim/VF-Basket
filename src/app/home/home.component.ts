6f8a065b0ec2b4f30874f26ab07e30894f49ef75
Rizwan Rahim <rizwanrahim@Live.com>
2024-02-18 22:49:50 +0530

    change to pull

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { environment } from '../../env/env';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-home',
