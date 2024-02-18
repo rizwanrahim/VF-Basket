e44249e59a10d1f4ea1c6652c3f90a8ad4cad8e3
Rizwan Rahim <rizwanrahim@users.noreply.github.com>
2024-02-19 00:14:56 +0530

    Enable google auth (#21)

---
+++ scripts/env-setup.js
@@ -0,0 +1,25 @@
+/* server.js in root directory */
+const fs = require('fs');
+const path = require('path');
+
+const dir = "src/environments";
+const file = "environment.ts";
+const prodFile = "environment.ts"; // For production deployment
+
+const content = `${process.env.FIREBASE}`;
+
+fs.access(dir, fs.constants.F_OK, (err) => {
+    try {
+        fs.writeFileSync(dir + "/" + file, content);
+        fs.writeFileSync(dir + "/" + prodFile, content);
+        console.log("created successfully in", process.cwd());
+        if (fs.existsSync(dir + "/" + file)) {
+            console.log("File is created", path.resolve(dir + "/" + file));
+            const str = fs.readFileSync(dir + "/" + file).toString();
+            console.log(str);
+        }
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+});
