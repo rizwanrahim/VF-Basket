8c5f8fc5cb7a46db1b1ffee92b5f53f4a93b6de7
Rizwan Rahim <rizwanrahim@Live.com>
2024-02-18 21:02:44 +0530

    change to pull

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     firebase: {
-        projectId: 'GITHUB_SECRET',
-        appId: 'GITHUB_SECRET',
-        storageBucket: 'GITHUB_SECRET',
-        apiKey: 'GITHUB_SECRET',
-        authDomain: 'GITHUB_SECRET',
-        messagingSenderId: 'GITHUB_SECRET',
-        measurementId: 'GITHUB_SECRET'
+        projectId: 'GITHUB_PROJECT_ID',
+        appId: 'GITHUB_APP_ID',
+        storageBucket: 'GITHUB_STORAGE_BUCKET',
+        apiKey: 'GITHUB_API_KEY',
+        authDomain: 'GITHUB_AUTH_DOMAIN',
+        messagingSenderId: 'GITHUB_MESSAGING_SENDER_ID',
+        measurementId: 'GITHUB_MEASUREMENT_ID'
     }
 }
