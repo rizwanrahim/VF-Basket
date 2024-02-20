fdab4e26baee0c110df1196e71e98031ef7f9332
Rizwan Rahim <rizwanrahim@Live.com>
2024-02-20 17:59:30 +0530

    list products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DatabaseService } from '../../services/firebase/database.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-products',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './admin-products.component.html',
   styleUrl: './admin-products.component.css'
 })
 export class AdminProductsComponent {
-
+  products$: any;
+  constructor(private readonly db: DatabaseService) {
+    this.db.getAllProduct().subscribe(products => {
+      this.products$ = products
+      console.log(products)
+    });
+  }
 }
