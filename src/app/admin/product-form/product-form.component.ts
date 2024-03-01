ed5ac6745c38f393faf44774cd9face9221df8b4
Rizwan Rahim <rizwan.mfrahim@outlook.com>
2024-03-01 12:32:47 +0530

    enable edit products

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { DatabaseService } from '../../services/firebase/database.service';
 import { CommonModule } from '@angular/common';
-import { Observable, map } from 'rxjs';
-import { KeyValue } from '../../models/KeyValue';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { take, map } from 'rxjs';
 
 @Component({
   selector: 'app-product-form',
@@ -15,15 +14,42 @@ import { Router } from '@angular/router';
 })
 export class ProductFormComponent {
   categories$:any
-  
+  product: any = {};
+  id: any;
+
   constructor(
     private readonly db: DatabaseService,
-    private readonly router: Router) {
+    private readonly router: Router,
+    private readonly route: ActivatedRoute) {
+    
+    this.PopulateCategories();
+    this.InitializeUpdateForm();
+  }
+
+  private PopulateCategories() {
     this.db.getCategory().subscribe(res => this.categories$ = res);
   }
 
+  private InitializeUpdateForm() {
+    this.id = this.route.snapshot.paramMap.get("id");
+    if (this.id) this.db.getProduct(this.id).pipe(map(item => ({ key: item.payload.key, ...item.payload.val() as any })))
+      .subscribe(product$ => this.product = product$);
+  }
+
   save(product: any) {
-    this.db.saveProduct(product);
+    if(this.id) this.db.update(this.id, product)
+    else this.db.saveProduct(product);
+    
+    this.router.navigate(['/admin/products']);
+  }
+
+  delete() {
+    if(!confirm('Are you sure you want to delete this product?')) return;
+    this.db.delete(this.id);
+    this.router.navigate(['/admin/products']);
+  }
+
+  cancel() {
     this.router.navigate(['/admin/products']);
   }
 }
